8261e0e3b65ee41ed2d991661ce8b58adc012acb
Nejat Boy <nejatboy34@gmail.com>
2025-07-29 22:40:43 +0300

    Component development.

--- HikingApp/Component/CardView.swift
+++ HikingApp/Component/CardView.swift
@@ -8,26 +8,54 @@
 import SwiftUI
 
 
+//MARK: CardView
 struct CardView: View {
+    
+    
     var body: some View {
         ZStack {
-            circle
-            image
+            CircleView()
+            ImageView()
         }
     }
+}
+
+
+//MARK: CustomCircleView
+private struct CircleView: View {
+    
+    @State private var isAnimateGradient = false
     
     
-    private var circle:  some View {
+    var body: some View {
         let colors: [Color] = [.colorIndigoMedium, .colorSalmonLight]
-        let gradient = LinearGradient(colors: colors, startPoint: .topLeading, endPoint: .bottomTrailing)
+        
+        let gradient = LinearGradient(
+            colors: colors,
+            startPoint: isAnimateGradient ? .topLeading : .bottomLeading,
+            endPoint: isAnimateGradient ? .bottomTrailing : .topTrailing
+        )
+        
+        let animation: Animation = .linear(duration: 3)
+            .repeatForever(autoreverses: true)
         
         return Circle()
             .fill(gradient)
+            .onAppear {
+                withAnimation(animation) {
+                    isAnimateGradient.toggle()
+                }
+            }
             .frame(width: 256, height: 256, alignment: .center)
     }
+}
+
+
+
+//MARK: ImageView
+private struct ImageView: View {
     
-    
-    private var image: some View {
+    var body: some View {
         Image("image-1").resizable().scaledToFit()
     }
 }
