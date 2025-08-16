21266810f5d8f6eecec2151146ebc9d10c6336ed
Nejat Boy <nejatboy34@gmail.com>
2025-08-16 15:21:32 +0300

    MotionAnimationView development.

--- HikingApp/Component/MotionAnimationView.swift
+++ HikingApp/Component/MotionAnimationView.swift
@@ -9,12 +9,30 @@ import SwiftUI
 
 struct MotionAnimationView: View {
     
+    @State private var randomCircle = Int.random(in: 6...12)
     
     
     var body: some View {
         ZStack {
-            Circle().foregroundColor(.white)
+            ForEach(0...randomCircle, id: \.self) { item in
+                circle
+            }
+
         }.frame(width: 256, height: 256)
+    }
+    
+    
+    var circle: some View {
+        let position: CGPoint = .init(x: randomCoordinate, y: randomCoordinate)
+        
+        return Circle()
+            .foregroundStyle(.white)
+            .opacity(0.25)
+            .position(position)
+    }
+    
+    var randomCoordinate: CGFloat {
+        .random(in: 0...256)
     }
 }
 
